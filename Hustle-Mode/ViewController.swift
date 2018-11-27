84f44d238d6dc236ba6798e2d0f04e6f2068dc68
Chris <codemedarkone@gmail.com>
2018-11-26 18:40:05 -0700

    added images and rocket and animated the rocket.

--- Hustle-Mode/ViewController.swift
+++ Hustle-Mode/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLabel: UILabel!
+    @IBOutlet weak var onLabel: UILabel!
+    
+    var player: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
     }
-
+    
+    
+    @IBAction func powerButtonPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerButton.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 100, width: 375, height: 402)
+        }) { (finished) in
+            self.hustleLabel.isHidden = false
+            self.onLabel.isHidden = false
+        }
+    }
+    
 
 }
 
